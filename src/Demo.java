b51cc45d25fd97ca4511657bbf997472f5ab15ba
YMARTINEZJ <yrmj@bu.edu>
2020-09-07 20:59:32 -0400

    First Comments

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Demo {
-    public static void main(String[] args) {
-        
+
+    public static int factorial(int n){
+        if(n == 0) return 1;
+        return n * factorial(n-1);
     }
+
+    public static int fib(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        int input = scanner.nextInt();
+
+        System.out.println(factorial(input));
+//        for(int i = input; i >=0; i--){
+//            System.out.println(i + " : " + fib(i));
+//        }
+
+        System.out.println("--------------");
+    }
+
 }
